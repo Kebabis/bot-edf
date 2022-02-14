87ad9e6eb8cb63dfb748ca5bfa6582d7eb40b308
Kebabis <eunaoseicara4@gmail.com>
2022-02-13 21:45:51 -0800

    base de dados feita

--- index.js
+++ index.js
@@ -15,8 +15,28 @@ client.once("ready", () => {
 });
 
 client.on("messageCreate", async (mensagem) => {
+  if (mensagem.content.startsWith("!bal")) {
+    const dados = banco.procurar(mensagem.author.id);
+    const embed = new MessageEmbed()
+      .setTitle("Banco")
+      .setColor("#668099")
+      .setDescription(`O saldo de <@${dados.id}> é de R$ ${dados.dinheiro},00`);
+    mensagem.channel.send({ embeds: [embed] });
+  }
+  if (mensagem.content.startsWith("!trabalhar")) {
+    var ganhos = banco.dinheiros(true, mensagem.author.id);
+    const dados = banco.procurar(mensagem.author.id);
+    const embed = new MessageEmbed()
+      .setTitle("Trabalho")
+      .setColor(0x00ae86)
+      .setDescription(
+        `<@${dados.id}> trabalhou em uma lojinha e ganhou R$ ${ganhos},00`
+      );
+    mensagem.channel.send({ embeds: [embed] });
+  }
   if (mensagem.content.startsWith("!registrar")) {
-    banco(mensagem.author);
+    banco.registrar(mensagem.author);
+    mensagem.channel.send("Registrado com sucesso!");
   }
   if (mensagem.content.startsWith("!apostar")) {
     const separado = mensagem.content.split(" ");
